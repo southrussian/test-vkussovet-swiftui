18664a0d2011b4d515486b5fccbd350646656f4e
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-09-25 11:53:26 +0300

    Initial Commit

---
+++ test-vkus-sovet-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test-vkus-sovet-swiftui
+//
+//  Created by Danil Peregorodiev on 25.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test-vkus-sovet-swiftui/test_vkus_sovet_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_vkus_sovet_swiftuiApp.swift
+//  test-vkus-sovet-swiftui
+//
+//  Created by Danil Peregorodiev on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct test_vkus_sovet_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
